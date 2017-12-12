b233569fc1875a866ee2c37042ff456c651b2fc2
Aga <agasoltysik@o2.pl>
2017-12-12 14:23:33 +0100

    StartGameLayout and StartGameController were added

---
+++ src/main/java/pl/edu/agh/to2/russianBank/ui/StartGameController.java
@@ -0,0 +1,35 @@
+package pl.edu.agh.to2.russianBank.ui;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.*;
+import javafx.scene.image.Image;
+import javafx.scene.input.MouseEvent;
+
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class StartGameController implements Initializable {
+
+    @FXML
+    public ImageView imageView1;
+
+    @FXML
+    public ImageView imageView2;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    @FXML
+    public void changeCard(MouseEvent mouseEvent) throws IOException {
+
+    }
+
+}
