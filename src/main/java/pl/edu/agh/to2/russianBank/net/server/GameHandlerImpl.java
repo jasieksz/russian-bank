9cfd0aff0590bed869b1fdd21c5225566ec54593
mswiate <marekswiatek1996@gmail.com>
2017-12-13 22:37:11 +0100

    room matching & room handling

--- src/main/java/pl/edu/agh/to2/russianBank/net/server/GameHandlerImpl.java
+++ src/main/java/pl/edu/agh/to2/russianBank/net/server/GameHandlerImpl.java
@@ -2,26 +2,32 @@ package pl.edu.agh.to2.russianBank.net.server;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import pl.edu.agh.to2.russianBank.net.room.Room;
+import pl.edu.agh.to2.russianBank.net.room.RoomMatcher;
 import pl.edu.agh.to2.russianBank.net.transport.Message;
+import pl.edu.agh.to2.russianBank.net.transport.ShutdownMessage;
 
 public class GameHandlerImpl implements GameHandler {
     private static final Logger LOG = LogManager.getLogger();
+    private final RoomMatcher roomMatcher = new RoomMatcher();
 
     @Override
     public void onConnect(PlayerConnection player) {
-        // TODO
         LOG.debug("Incoming player {}", player);
+        roomMatcher.assign(player);
     }
 
     @Override
     public void onClose(PlayerConnection player) {
-        // TODO
         LOG.debug("Closing player {}", player);
+        Room room = roomMatcher.getRoom(player);
+        room.unicast(player, new ShutdownMessage());
+        roomMatcher.deleteRoom(room);
     }
 
     @Override
     public void onMessage(PlayerConnection player, Message message) {
-        // TODO
         LOG.debug("Message from player {}: {}", player, message);
+        // TODO
     }
 }
