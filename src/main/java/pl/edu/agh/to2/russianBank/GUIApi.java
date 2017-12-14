b6a8d9e164f5cd573a12dda26b144661a8a48e97
Magdalena Nowak <madzia.nowak222@poczta.fm>
2017-12-14 19:43:49 +0100

    Pane to ImageView

--- src/main/java/pl/edu/agh/to2/russianBank/GUIApi.java
+++ src/main/java/pl/edu/agh/to2/russianBank/GUIApi.java
@@ -1,13 +1,9 @@
 package pl.edu.agh.to2.russianBank;
 
-/**
- * Created by Madzia on 05.12.2017.
- */
-public interface Client {
-    /*void onStartGame(State initialState);
-    void onTurnChanged(Player newPlayer);
-    void moveCard(Player player, Slot previousSlot, Slot newSlot);
-    void onCardMoved(Player player, Slot previousSlot, Slot newSlot, Optional<Card> card);
-    void knock();
+
+public interface GUIApi {
+    /*void startGame(GameTable table);
+
+    void movingCard(Player player, Slot previousSlot, Slot newSlot);
     */
 }
