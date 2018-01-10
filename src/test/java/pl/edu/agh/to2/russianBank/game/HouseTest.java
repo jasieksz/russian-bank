ad4e3896c6c0ffa433b09763c178fdf585e98549
Jasiek <jasiek.sznajd@gmail.com>
2018-01-10 19:19:18 +0100

    implementing move cases

---
+++ src/test/java/pl/edu/agh/to2/russianBank/game/HouseTest.java
@@ -0,0 +1,12 @@
+package pl.edu.agh.to2.russianBank.game;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.Assert.assertTrue;
+
+public class HouseTest {
+}
