865bb6dd6940d93914bad30d895905742f47590e
Magdalena Nowak <madzia.nowak222@poczta.fm>
2018-01-12 00:00:47 +0100

    some cards added

--- src/main/java/pl/edu/agh/to2/russianBank/game/Hand.java
+++ src/main/java/pl/edu/agh/to2/russianBank/game/Hand.java
@@ -26,7 +26,9 @@ public class Hand extends ICardSet {
 
     @Override
     public boolean putCard(Card card) {
-        return false;
+        //return false;
+        cards.add(card);
+        return true;
     }
 
     @Override
