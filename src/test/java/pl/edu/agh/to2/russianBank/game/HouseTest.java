182ae2a81d7188d2a348effc61b98c18c90955d8
Jasiek <jasiek.sznajd@gmail.com>
2018-01-12 13:47:57 +0100

    *When creating game, 4 top card from each Hand move into Houses *Started HouseTest

--- src/test/java/pl/edu/agh/to2/russianBank/game/HouseTest.java
+++ src/test/java/pl/edu/agh/to2/russianBank/game/HouseTest.java
@@ -9,4 +9,19 @@ import java.util.Optional;
 import static org.junit.Assert.assertTrue;
 
 public class HouseTest {
+
+    private Card card1 = new Card(CardSuit.HEARTS, CardRank.ACE);
+    private Card card2 = new Card(CardSuit.HEARTS, CardRank.CARD_2);
+    private Card card3 = new Card(CardSuit.HEARTS, CardRank.KING);
+
+    @Test
+    public void takeTopCardTest(){
+        List<Card> cardList = new ArrayList<>();
+        cardList.add(card1);
+        cardList.add(card2);
+        House house = new House(cardList);
+        assertTrue(house.takeTopCard().equals(Optional.of(card2)));
+    }
+
+
 }
