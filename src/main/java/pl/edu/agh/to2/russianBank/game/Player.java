9169fb20d8190d77ff606ca321add45e18a90b55
Marek Kaput <marek.kaput@outlook.com>
2018-01-26 13:18:51 +0100

    don't duplicate hand & waste

--- src/main/java/pl/edu/agh/to2/russianBank/game/Player.java
+++ src/main/java/pl/edu/agh/to2/russianBank/game/Player.java
@@ -11,34 +11,13 @@ import java.util.stream.Collectors;
 public class Player {
 
     private String name;
-    private PlayerDeck playerDeck;
 
     public Player (String name){
         this.name = name;
-        List<Card> tmp = new ArrayList<>();
-        for (CardSuit suit : CardSuit.values()) {
-            for (CardRank rank : CardRank.values()) {
-                tmp.add(new Card(suit, rank));
-            }
-        }
-        Collections.shuffle(tmp);
-        tmp = tmp.stream().filter(card -> card.getRank().getRank() <= 3).collect(Collectors.toList());
-        System.out.println("TMP SIZE IN PLAYER : " + Integer.toString(tmp.size()));
-        this.playerDeck = new PlayerDeck(new Hand(tmp), new Waste());
-    }
-
-
-    public Player(String name, PlayerDeck playerDeck) {
-        this.name = name;
-        this.playerDeck = playerDeck;
     }
 
     public String getName() {
         return name;
-    }
-
-    public PlayerDeck getPlayerDeck() {
-        return playerDeck;
     }
 
     @Override
@@ -46,21 +25,19 @@ public class Player {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Player player = (Player) o;
-        return Objects.equals(name, player.name) &&
-                Objects.equals(playerDeck, player.playerDeck);
+        return Objects.equals(name, player.name);
     }
 
     @Override
     public int hashCode() {
 
-        return Objects.hash(name, playerDeck);
+        return Objects.hash(name);
     }
 
     @Override
     public String toString() {
         return MoreObjects.toStringHelper(this)
                 .add("name", name)
-                .add("playerDeck", playerDeck)
                 .toString();
     }
 }
